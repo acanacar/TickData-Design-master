f9b02b53f744bbc903dfb5a4b3ccd0a49fef6d0e
acanacar <ahmetcanacar.1@gmail.com>
2019-11-01 16:14:39 +0300

    VaR directory is added

---
+++ VaR/constants.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+# hist_store = str(
+#     '/home/acanacar/Desktop/projects/pycharm/TickData-Design-master-master/temporary_analysis/Value_at_Risk/hist_data.h5')
+
+hist_store = str(
+    r'C:\Users\a.acar\PycharmProjects\Advanced-Deep-Trading-master\analysistemporary\VaR\hist_data.h5')
+
+
+def Tickers():
+    tickers = [
+        'EURUSD=X', 'EURTRY=X', 'TRY=X',
+        'XU100.IS', 'XU030.IS',
+        'AKBNK.IS',
+        'ARCLK.IS',
+        'ASELS.IS',
+        'BIMAS.IS',
+        'DOHOL.IS',
+        'EKGYO.IS',
+        'EREGL.IS',
+        'FROTO.IS',
+        'GARAN.IS',
+        'HALKB.IS',
+        'ISCTR.IS',
+        'KCHOL.IS',
+        'KOZAA.IS',
+        'KOZAL.IS',
+        'KRDMD.IS',
+        'PETKM.IS',
+        'PGSUS.IS',
+        'SAHOL.IS',
+        'SISE.IS',
+        'SODA.IS',
+        'TAVHL.IS',
+        'TCELL.IS',
+        'THYAO.IS',
+        'TKFEN.IS',
+        'TOASO.IS',
+        'TTKOM.IS',
+        'TUPRS.IS',
+        'VAKBN.IS',
+        'YKBNK.IS']
+    return tickers
